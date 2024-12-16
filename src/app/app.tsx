f2450f29398b430a4df6b424306edc69eb34752b
jerome-bienaime <jerome.bienaime@gmail.com>
2024-12-16 14:10:34 +0100

    :lipstick: Add github link

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,19 +5,32 @@ import { FilterBar } from "./_components/filter_bar";
 import _ from "lodash";
 import { useCardStore } from "./stores/card_store";
 import { useEffect } from "react";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
 
 export function App({ cards }: { cards: any }) {
   const merge = useCardStore((state) => state.merge);
   const cardStore = useCardStore((state) => state.cards);
 
   useEffect(() => {
-      merge(cards);
+    merge(cards);
   }, [merge]);
 
   return (
     <main>
       <Box display="flex" my="3" justifyContent={"center"}>
-        <Image src="./logo.png" alt="Mindbug" width="128px" height="auto" />
+        <Box display="flex" justifyContent={"center"} width="100%">
+          <Image src="./logo.png" alt="Mindbug" width="128px" height="auto" />
+        </Box>
+
+        <Box sx={{ ml: "auto", mr: 2 }}>
+          <Link
+            href="https://github.com/jerome-bienaime/mindbug-cards"
+            target="_blank"
+          >
+            <FaGithub fontSize={"24px"} />
+          </Link>
+        </Box>
       </Box>
       <FilterBar />
       <Grid templateColumns="repeat(12, 1fr)" gap={1} mx={1}>
